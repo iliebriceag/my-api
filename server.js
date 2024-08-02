6d45fe86ceed7f11c8bbe7a8ccbf5ef49be8a41f
Teri01 <briceag.ilie97@gmail.com>
2024-08-02 11:58:16 +0300

    a

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+// const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-// Servește fișiere statice din directorul 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+// // Servește fișiere statice din directorul 'public'
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Endpoint pentru a servi fișierul JSON
-app.get('/api/players', (req, res) => {
-    fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Eroare la citirea fișierului JSON');
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
-});
+// // Endpoint pentru a servi fișierul JSON
+// app.get('/api/players', (req, res) => {
+//     fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Eroare la citirea fișierului JSON');
+//         } else {
+//             res.json(JSON.parse(data));
+//         }
+//     });
+// });
 
-app.listen(PORT, () => {
-    console.log(`Serverul rulează la http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Serverul rulează la http://localhost:${PORT}`);
+// });
