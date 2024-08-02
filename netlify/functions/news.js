2fb001503243499c049eafdafb79b6a369efae23
Teri01 <briceag.ilie97@gmail.com>
2024-08-02 13:29:11 +0300

    last update

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -9,7 +9,7 @@ exports.handler = async function(event, context) {
             "title": "Goluri și Realizări",
             "description": "Cristiano Ronaldo este cunoscut pentru abilitatea sa incredibilă de a marca goluri și pentru realizările sale impresionante în fotbal.",
             "data": "2024-03-21",
-            "imagine": "https://cdn.pixabay.com/photo/2023/11/07/20/24/cristiano-ronaldo-8373364_1280.jpg"
+            "image": "https://cdn.pixabay.com/photo/2023/11/07/20/24/cristiano-ronaldo-8373364_1280.jpg"
         },
         {
             "id": 2,
@@ -17,7 +17,7 @@ exports.handler = async function(event, context) {
             "title": "Dribling și Creativitate",
             "description": "Lionel Messi este recunoscut pentru driblingul său excelent și creativitatea sa pe teren.",
             "data": "2024-02-01",
-            "imagine": "https://cdn.pixabay.com/photo/2017/01/25/17/20/lionel-2008448_1280.jpg"
+            "image": "https://cdn.pixabay.com/photo/2017/01/25/17/20/lionel-2008448_1280.jpg"
         },
         {
             "id": 3,
@@ -25,7 +25,7 @@ exports.handler = async function(event, context) {
             "title": "Tehnică și Viteză",
             "description": "Neymar Jr. este un jucător de fotbal remarcabil datorită tehnicii sale rafinate și vitezei sale extraordinare.",
             "data": "2024-08-11",
-            "imagine": "https://cdn.pixabay.com/photo/2016/02/15/18/50/brazil-germany-1201762_960_720.jpg"
+            "image": "https://cdn.pixabay.com/photo/2016/02/15/18/50/brazil-germany-1201762_960_720.jpg"
         },
         {
             "id": 4,
@@ -33,7 +33,7 @@ exports.handler = async function(event, context) {
             "title": "Tineret și Viteză",
             "description": "Kylian Mbappe este un star tânăr în fotbal, cunoscut pentru viteza și agilitatea sa.",
             "data": "2024-08-13",
-            "imagine": "https://cdn.pixabay.com/photo/2022/06/08/10/00/kylian-mbappe-7250181_1280.jpg"
+            "image": "https://cdn.pixabay.com/photo/2022/06/08/10/00/kylian-mbappe-7250181_1280.jpg"
         },
         {
             "id": 5,
@@ -41,7 +41,7 @@ exports.handler = async function(event, context) {
             "title": "Perseverență și Talent",
             "description": "Mohamed Salah a devenit unul dintre cei mai buni jucători din lume prin perseverența și talentul său.",
             "data": "2024-05-30",
-            "imagine": "https://i.pinimg.com/originals/58/83/ab/5883ab7cb651a251b364c15609a81a4f.jpg"
+            "image": "https://i.pinimg.com/originals/58/83/ab/5883ab7cb651a251b364c15609a81a4f.jpg"
         },
         {
             "id": 6,
@@ -49,119 +49,119 @@ exports.handler = async function(event, context) {
             "title": "Apărare și Lider",
             "description": "Sergio Ramos este un apărător de top și un lider pe teren.",
             "data": "2024-08-01",
-            "imagine": "https://w0.peakpx.com/wallpaper/40/165/HD-wallpaper-sergio-ramos-real-madrid-sergio-ramos-spain-thumbnail.jpg"
+            "image": "https://w0.peakpx.com/wallpaper/40/165/HD-wallpaper-sergio-ramos-real-madrid-sergio-ramos-spain-thumbnail.jpg"
         },
         {
             "id": 7,
             "name": "Luka Modric",
             "title": "Mijlocaș și Viziune",
             "description": "Luka Modric este un mijlocaș excelent cunoscut pentru viziunea sa pe teren și abilitatea de a controla jocul.",
-            "data": "2024-08-01",
-            "imagine": "https://i.pinimg.com/originals/fe/4c/c3/fe4cc3021a48226100248c0acc7de29b.jpg"
+            "data": "2024-03-05",
+            "image": "https://i.pinimg.com/originals/fe/4c/c3/fe4cc3021a48226100248c0acc7de29b.jpg"
         },
         {
             "id": 8,
             "name": "Virgil van Dijk",
             "title": "Apărare și Forță",
             "description": "Virgil van Dijk este cunoscut pentru forța și abilitatea sa defensivă.",
-            "data": "2024-08-01",
-            "imagine": "https://i.pinimg.com/originals/c2/f6/43/c2f643949d8c40f6de68c8f829c79c6b.jpg"
+            "data": "2024-01-01",
+            "image": "https://i.pinimg.com/originals/c2/f6/43/c2f643949d8c40f6de68c8f829c79c6b.jpg"
         },
         {
             "id": 9,
             "name": "Kevin De Bruyne",
             "title": "Creativitate și Pasare",
             "description": "Kevin De Bruyne este un mijlocaș creativ, renumit pentru pasele și viziunea sa.",
-            "data": "2024-08-01",
-            "imagine": "https://i.pinimg.com/originals/5b/97/d1/5b97d1314acb51869e53a05700a9f3ff.jpg"
+            "data": "2024-10-17",
+            "image": "https://i.pinimg.com/originals/5b/97/d1/5b97d1314acb51869e53a05700a9f3ff.jpg"
         },
         {
             "id": 10,
             "name": "Robert Lewandowski",
             "title": "Marcator și Tehnică",
             "description": "Robert Lewandowski este unul dintre cei mai buni atacanți din lume, cunoscut pentru tehnica și abilitatea sa de a marca.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/robert_lewandowski.jpg"
+            "data": "2024-11-02",
+            "image": "https://i.pinimg.com/originals/53/db/6b/53db6b5d4bd4808013321fb75e24c2d8.jpg"
         },
         {
             "id": 11,
             "name": "Eden Hazard",
             "title": "Agilitate și Dribling",
             "description": "Eden Hazard este un jucător talentat cunoscut pentru agilitatea și driblingul său.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/eden_hazard.jpg"
+            "data": "2024-05-09",
+            "image": "https://i.pinimg.com/originals/f2/68/5b/f2685b0b031c611b9617d286419c533e.jpg"
         },
         {
             "id": 12,
             "name": "Antoine Griezmann",
             "title": "Versatilitate și Tehnică",
             "description": "Antoine Griezmann este cunoscut pentru versatilitatea și tehnica sa pe teren.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/antoine_griezmann.jpg"
+            "data": "2024-01-21",
+            "image": "https://i.pinimg.com/originals/18/02/c8/1802c8850e309c889baf9283f7fbe4ac.jpg"
         },
         {
             "id": 13,
             "name": "Paulo Dybala",
             "title": "Creativitate și Viteză",
             "description": "Paulo Dybala este un jucător creativ și rapid.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/paulo_dybala.jpg"
+            "data": "2024-03-30",
+            "image": "https://i.pinimg.com/originals/fb/38/c8/fb38c8ced0334593b05b4431b84214a4.jpg"
         },
         {
             "id": 14,
             "name": "Sadio Mane",
             "title": "Viteză și Dribling",
             "description": "Sadio Mane este cunoscut pentru viteza și driblingul său impresionant.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/sadio_mane.jpg"
+            "data": "2024-02-11",
+            "image": "https://i.pinimg.com/originals/92/1c/3d/921c3d97ed11a6b0e3cf66dc9ce90c0a.jpg"
         },
         {
             "id": 15,
             "name": "Paul Pogba",
             "title": "Forță și Creativitate",
             "description": "Paul Pogba este un mijlocaș puternic și creativ.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/paul_pogba.jpg"
+            "data": "2024-09-16",
+            "image": "https://i.pinimg.com/originals/3b/db/c6/3bdbc655d31cd65d2dc01bdaee7f2ea2.jpg"
         },
         {
             "id": 16,
             "name": "Romelu Lukaku",
             "title": "Forță și Goluri",
             "description": "Romelu Lukaku este cunoscut pentru forța și abilitatea sa de a marca goluri.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/romelu_lukaku.jpg"
+            "data": "2024-19-08",
+            "image": "https://i.pinimg.com/originals/95/59/2b/95592b0a0c90f692d8681a2e5f987b17.png"
         },
         {
             "id": 17,
             "name": "Raheem Sterling",
             "title": "Viteză și Finalizare",
             "description": "Raheem Sterling este un jucător rapid și eficient în fața porții.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/raheem_sterling.jpg"
+            "data": "2024-07-11",
+            "image": "https://i.pinimg.com/originals/6a/7b/96/6a7b96b42b6938833c89609edb051537.png"
         },
         {
             "id": 18,
             "name": "Gareth Bale",
             "title": "Viteză și Tehnică",
             "description": "Gareth Bale este cunoscut pentru viteza și tehnica sa pe teren.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/gareth_bale.jpg"
+            "data": "2024-12-20",
+            "image": "https://i.pinimg.com/originals/4e/99/57/4e995732559455c4fd6881bcc8571bff.jpg"
         },
         {
             "id": 19,
             "name": "Harry Kane",
             "title": "Finalizare și Tehnică",
             "description": "Harry Kane este un atacant remarcabil, cunoscut pentru finalizarea și tehnica sa.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/harry_kane.jpg"
+            "data": "2024-02-28",
+            "image": "https://i.pinimg.com/originals/18/88/db/1888dbc341538dee07dcc25f2e64b1f1.jpg"
         },
         {
             "id": 20,
             "name": "Luis Suarez",
             "title": "Aggresivitate și Goluri",
             "description": "Luis Suarez este cunoscut pentru agresivitatea și abilitatea sa de a marca goluri.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/luis_suarez.jpg"
+            "data": "2024-01-21",
+            "image": "https://i.pinimg.com/originals/d8/ba/f6/d8baf6b3e65282c42e1b23ccb174280c.jpg"
         },
         {
             "id": 21,
@@ -169,23 +169,23 @@ exports.handler = async function(event, context) {
             "title": "Eleganță și Finalizare",
             "description": "Karim Benzema este cunoscut pentru eleganța și abilitatea sa de a finaliza.",
             "data": "2024-08-01",
-            "imagine": "https://example.com/images/karim_benzema.jpg"
+            "image": "https://i.pinimg.com/originals/fd/12/8e/fd128ec9fcd389802dc620da58bb0932.jpg"
         },
         {
             "id": 22,
             "name": "Toni Kroos",
             "title": "Control și Pasare",
             "description": "Toni Kroos este cunoscut pentru controlul său excelent al mingii și abilități de pasare.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/toni_kroos.jpg"
+            "data": "2024-02-12",
+            "image": "https://i.pinimg.com/originals/9f/13/20/9f13208c28e0ce2ca7382d3f3d9a4ca9.jpg"
         },
         {
             "id": 23,
             "name": "N'Golo Kante",
             "title": "Muncă și Interceptare",
             "description": "N'Golo Kante este cunoscut pentru etica sa de muncă și abilitatea de a intercepta pasele adversarilor.",
-            "data": "2024-08-01",
-            "imagine": "https://example.com/images/ngolo_kante.jpg"
+            "data": "2024-08-19",
+            "image": "https://i.pinimg.com/originals/c1/fe/b2/c1feb2206faa57dc40594cf3d42a46b0.jpg"
         }
     ];
 
