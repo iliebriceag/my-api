a2ebb7fcbad6daf26c9e1bb310e8b437724acdae
Teri01 <briceag.ilie97@gmail.com>
2024-08-02 09:56:30 +0300

    last

---
+++ netlify/functions/players.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async function(event, context) {
+    try {
+        const data = fs.readFileSync(path.join(__dirname, '../../data.json'), 'utf8');
+        return {
+            statusCode: 200,
+            body: data,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Eroare la citirea fișierului JSON' }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+};
